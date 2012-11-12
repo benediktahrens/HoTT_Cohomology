a6ce3f2c719a15c440801dffa67b275d6aa8aa1c
Benedikt Ahrens <benedikt.ahrens@gmx.net>
2012-11-11 20:18:25 -0500

    still missing smash_comp_contract

--- smash_product.v
+++ smash_product.v
@@ -15,6 +15,8 @@ Variables A B : pt_type.
 Reserved Notation "< A , B >"
  (at level 40, B at next level, left associativity).
 
+Check @map_dep.
+Check @map. About map.
 Record smash_data : Type := {
   smash_carrier : Type ;
   smash_pair : forall (a : A) (b : B), smash_carrier 
@@ -37,6 +39,72 @@ Record smash_data : Type := {
              d_base_2) 
      
       , forall x : smash_carrier, P x
+;
+  smash_comp_pair : forall (P : smash_carrier -> Type)
+     (d_pair : forall a b, P (< a , b>) )
+     (d_base_1 : P base_1)
+     (d_base_2 : P base_2)
+     (d_contract_1 : forall a : A,
+        transport (contract_1 a) (d_pair a (point B)) = 
+             d_base_1) 
+     (d_contract_2 : forall b : B,
+        transport (contract_2 b) (d_pair (point A) b) = 
+             d_base_2),
+       forall a b,
+       smash_elim  (*d_pair*) (*d_base_1*) (*d_base_2*) 
+          d_contract_1 d_contract_2
+            (<a, b>) = d_pair a b
+;
+
+  smash_comp_base_1 : forall (P : smash_carrier -> Type)
+     (d_pair : forall a b, P (< a , b>) )
+     (d_base_1 : P base_1)
+     (d_base_2 : P base_2)
+     (d_contract_1 : forall a : A,
+        transport (contract_1 a) (d_pair a (point B)) = 
+             d_base_1) 
+     (d_contract_2 : forall b : B,
+        transport (contract_2 b) (d_pair (point A) b) = 
+             d_base_2),
+
+       smash_elim  (*d_pair*) (*d_base_1*) (*d_base_2*) 
+          d_contract_1 d_contract_2
+            base_1 = d_base_1
+;
+ smash_comp_base_2 : forall (P : smash_carrier -> Type)
+     (d_pair : forall a b, P (< a , b>) )
+     (d_base_1 : P base_1)
+     (d_base_2 : P base_2)
+     (d_contract_1 : forall a : A,
+        transport (contract_1 a) (d_pair a (point B)) = 
+             d_base_1) 
+     (d_contract_2 : forall b : B,
+        transport (contract_2 b) (d_pair (point A) b) = 
+             d_base_2),
+
+       smash_elim  (*d_pair*) (*d_base_1*) (*d_base_2*) 
+          d_contract_1 d_contract_2
+            base_2 = d_base_2
+
+;
+
+  smash_comp_contract_1 : forall (P : smash_carrier -> Type)
+     (d_pair : forall a b, P (< a , b>) )
+     (d_base_1 : P base_1)
+     (d_base_2 : P base_2)
+     (d_contract_1 : forall a : A,
+        transport (contract_1 a) (d_pair a (point B)) = 
+             d_base_1) 
+     (d_contract_2 : forall b : B,
+        transport (contract_2 b) (d_pair (point A) b) = 
+             d_base_2),
+    forall a : A,
+      map_dep (smash_elim d_contract_1 d_contract_2)(contract_1 a) = 
+          map (transport (contract_1 a))
+       ( smash_comp_base_1 (d_contract_1) d_contract_2 )
+              @ d_contract_1 a @ 
+                ! smash_comp_base_1 (d_contract_1) d_contract_2 
+
 }.
 
 
